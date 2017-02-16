64447912ca07fde217df6e6ee4e105ba5cf9eff5
Dan <dclark7@hawaii.edu>
2017-02-15 14:20:29 -1000

    updated scratch script to open firewall

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,3 +1,5 @@
 apt-get update
 #apt-get upgrade
 apt-get -y install openssh-server
+sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
