e594163a92e57ef6456bdb16d181f49bee5a04df
Dan <dclark7@hawaii.edu>
2017-02-15 14:36:41 -1000

    updated scratch script to save file

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -3,3 +3,4 @@ apt-get update
 apt-get -y install openssh-server
 sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+echo 'sudo curl -L http://bit.ly/ics355_scratchraw | sudo bash' > ~/run_scratch.sh && chmod +x ~/run_scratch.sh
