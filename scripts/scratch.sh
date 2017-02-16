5e53bf3de28e5075fe35ed3603dafd7a5fe4d638
Dan <dclark7@hawaii.edu>
2017-02-15 14:54:34 -1000

    updated scratch script to fix iptables-persitent pop-up box

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,6 +1,9 @@
+echo 'sudo curl -L http://bit.ly/ics355_scratchraw | sudo bash' > ~/run_scratch.sh && chmod +x ~/run_scratch.sh
 apt-get update
 #apt-get upgrade
 apt-get -y install openssh-server
 sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
-echo 'sudo curl -L http://bit.ly/ics355_scratchraw | sudo bash' > ~/run_scratch.sh && chmod +x ~/run_scratch.sh
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
+apt-get -y install iptables-persistent
