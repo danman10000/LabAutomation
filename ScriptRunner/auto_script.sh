ed347ae3b8aa38028c9c13ca13ff5e7757fe939e
Dan <dclark7@hawaii.edu>
2017-10-02 13:21:48 -1000

    Includes many helper functions and examples

--- ScriptRunner/auto_script.sh
+++ ScriptRunner/auto_script.sh
@@ -1,3 +1,105 @@
-#Example auto shutdown script
+###############
+# Support Functions
+# Examples taken from here: https://github.com/ghoulmann/Raspliance-Core/blob/master/compile_tklpatch.sh
+install ()
+{
+	apt-get update
+	DEBIAN_FRONTEND=noninteractive apt-get -y \
+        -o DPkg::Options::=--force-confdef \
+        -o DPkg::Options::=--force-confold \
+        install $@
+}
+
+purge ()
+{
+	apt-get purge
+	DEBIAN_FRONTEND=noninteractive apt-get -y \
+        -o DPkg::Options::=--force-confdef \
+        -o DPkg::Options::=--force-confold \
+	purge $@
+}
+
+###############
+#Common Tasks
+###############
+#apt-get autoremove -y
+cd ~
+rm ~/nohup.out
+rm -f /var/crash/*
+sudo chown -R administrator:administrator ~/*
 sudo shutdown -h now
 exit
+###############
+
+###############
+#Setup Stuff
+###############
+#echo "ssh-rsa [put key here]" >> ~/.ssh/authorized_keys
+#sudo apt-get update
+#sudo apt-get -y install python-pip
+#pip install futures
+#sudo apt-get -y install python-bs4
+#sudo apt-get -y install python-scapy
+#mkdir ~/ics355
+#cp -r * ~/ics355
+###############
+
+
+###############
+# Install DanBot
+#sudo curl -L https://raw.githubusercontent.com/danman10000/IRC-Bot/master/setup.sh | bash
+###############
+
+
+##############
+#Raspberry Pi Rollout
+#sudo apt-get update
+#sudo apt-get install dcfldd
+##############
+
+
+##############
+#RDP
+#sudo apt-get -y install rdesktop
+##############
+
+
+##############
+#General Updates
+#sudo apt-get update
+#sudo apt-get upgrade -y
+##############
+
+################################################
+################################################
+#Demo Prep
+################################################
+################################################
+##############
+#Crypt Tools
+# gnupg
+##############
+#sudo apt-get -y install gnupg
+#sudo apt-get -y install pgpdump
+#sudo apt-get -y install gpa
+#sudo pip install python-gnupg
+#sudo apt-get -y install seahorse-nautilus
+#seahorse
+#seahorse-tool 
+#sudo apt-get install nautilus
+#nautilus
+##############
+#steghide
+##############
+#sudo apt-get -y install steghide
+#sudo apt-get -f -y install
+#sudo apt-get -y install openjdk-8-jre -f
+#wget https://github.com/syvaidya/openstego/releases/download/openstego-0.7.0/openstego_0.7.0-1_amd64.deb
+#sudo dpkg -i ./openstego_0.7.0-1_amd64.deb 
+#echo java -Xmx512m -jar /usr/share/openstego/lib/openstego.jar > ~/ics355_run_stego.sh
+#chmod +x ~/ics355_run_stego.sh
+##############
+#Hashing
+##############
+#sudo apt-get -y install gtkhash
+
