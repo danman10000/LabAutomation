c53fa2176f6c1cf81dd0ae7d717a855688361923
Dan <dclark7@hawaii.edu>
2017-10-17 13:52:41 -1000

    cleaned some of the duplication in the file

--- scripts/scratch.sh
+++ scripts/scratch.sh
@@ -1,10 +1,11 @@
+#http://bit.ly/ics355_scratchraw
 DATE=`date +%d-%m-%y` 
 echo 'sudo curl -L http://bit.ly/ics355_scratchraw | sudo bash' > ~/run_scratch.sh && chmod +x ~/run_scratch.sh
 apt-get update
 #apt-get upgrade
 apt-get -y install openssh-server
-sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+#sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+#sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
 apt-get -y install iptables-persistent
@@ -12,3 +13,5 @@ sudo cp /etc/iptables/rules.v4 /etc/iptables/rules.v4.bak_$DATE
 sudo iptables -A INPUT -p tcp -s 192.168.168.250 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 sudo iptables-save | sudo tee /etc/iptables/rules.v4
+cp ~/authorized_keys ~/authorized_keys_Backup
+cat id_rsa.pub >> ~/authorized_keys
