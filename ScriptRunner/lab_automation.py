088c749a9895fa7a8baeed2ec3c87e078e48e577
Dan <dclark7@hawaii.edu>
2017-12-11 14:50:07 -1000

    Fixed some multithreading issues with printing out error codes. Also added better exception handling for the SSH connect function

--- ScriptRunner/lab_automation.py
+++ ScriptRunner/lab_automation.py
@@ -1,4 +1,5 @@
-# Based on https://pynet.twb-tech.com/blog/python/paramiko-ssh-part1.html
+# Based: https://pynet.twb-tech.com/blog/python/paramiko-ssh-part1.html
+# Threading Reference: https://stackoverflow.com/questions/3485428/creating-multiple-ssh-connections-at-a-time-using-paramiko
 import paramiko
 import time
 import ConfigParser
@@ -7,6 +8,21 @@ import os.path
 import traceback
 import base64
 import threading
+import sys, os, string
+import logging
+import socket
+
+#logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
+class patched_SSHClient(paramiko.SSHClient):
+    def _auth(self, username, password, *args):
+        if not password:
+            try:
+                self._transport.auth_none(username)
+                return
+            except paramiko.BadAuthenticationType:
+                pass
+        paramiko.SSHClient._auth(self, username, password, *args)
 
 #USER_CONFIG="administrator"
 USER_CONFIG="administrator"
@@ -30,7 +46,12 @@ USERTYPES={"root":ROOT,"user":USER}
 IP_RANGE_A=192
 IP_RANGE_B=168
 IP_RANGE_C=168
-IP_RANGE_D=[1,35]
+IP_RANGE_D=[1,30] #Dells
+#IP_RANGE_D=[31,60] #PIs
+#IP_RANGE_D=10 #Direct
+
+#Threading output semaphore
+outlock = threading.Lock()
 
 #Expand out a list similiar to the range function put make it IP friendly
 # lRange = Either a list of 2 elements corrisponding the lower and upper bound of a IP space or a single integer.
@@ -41,9 +62,10 @@ def get_range(lRange):
         #Note that Range is not inclusive so add 1
         return [str(x) for x in range(lRange[0],lRange[1]+1)]
     else:
-        return "Error: IP setup incorrect"
+        print "Error: IP setup incorrect"
         exit()
 
+#Generate a list of IPs based on the ranges.
 def get_ip_list():
     lTarget_IPs=[] 
     dot = "."
@@ -76,125 +98,138 @@ def disable_paging(remote_conn):
 
     return output
 
-def do_ssh_runcmd():
+def Exec_per_ip():
+    lThreads =[]
+    
     with open(SCRIPT_TO_RUN,"r") as fScript:
         sScript=fScript.readlines()
     
     sTmpFileName=str(uuid.uuid1())+".sh"
     for ip in get_ip_list():
-        print "########### IP:",ip
-        username,password,usertype = get_creds(USER_CONFIG)
-
-        # Create instance of SSHClient object
-        remote_conn_pre = paramiko.SSHClient()
-
-        # Automatically add untrusted hosts (make sure okay for security policy in your environment)
-        remote_conn_pre.set_missing_host_key_policy(
-             paramiko.AutoAddPolicy())
-
-        try:
-
-            # initiate SSH connection
-            remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=True, allow_agent=False, timeout=1)
-            print "SSH connection established to %s" % ip
-
-            # Use invoke_shell to establish an 'interactive session'
-            remote_conn = remote_conn_pre.invoke_shell()
-            print "Interactive SSH session established"
-
-            # Strip the initial router prompt
-            output = remote_conn.recv(1000)
-
-            # See what we have
-            print output
-
-            # Turn off paging
-            #disable_paging(remote_conn)
-
-            remote_conn.send("\n")
-            if USE_TEMP_FILE:
-                bInMultiLineComment=False
-                for sLine in sScript:
-                    if sLine.startswith("'''"): 
-                        bInMultiLineComment= not bInMultiLineComment
-                    if not sLine.startswith("#") and not sLine.startswith("'''") and not len(sLine.strip())<=1 and bInMultiLineComment==False:
-                        # Now let's try to send a command
-                        print "Sending: " + "echo '" + sLine.strip() + "' >> " + os.path.join(TEMP_DIR,sTmpFileName)
-                        remote_conn.send("echo '" + sLine.strip() + "' >> " + os.path.join(TEMP_DIR,sTmpFileName) + "\n")
-
-                        # Wait for the command to complete
-                        time.sleep(.3)
-                    
-                        output = remote_conn.recv(5000)
-                        print output
+        myThread = threading.Thread(target=do_ssh_runcmd, args=(ip,sScript,sTmpFileName,)) #do_ssh_runcmd(ip,sScript,outlock)
+        myThread.start()
+        lThreads.append(myThread)
+    
+    with outlock:    
+        print "^"*75
+        print "All threads started... waiting to finish"
+        print "^"*75
+    for t in lThreads:
+        t.join()
+        
             
-                sShCommand="nohup sh " + os.path.join(TEMP_DIR,sTmpFileName) + " && rm " + os.path.join(TEMP_DIR,sTmpFileName) + " && exit\n"
-            else:
-                #send the date command as the first command so that you know when it was started.
-                sRunCmd="date"
-                for sLine in sScript:
-                    if not sLine.startswith("#") and not len(sLine.strip())<=1:
-                        sRunCmd=sRunCmd + " && " + sLine.strip()
-                #Due the variety of escapes and things in scripts you need to base64 it before sending it over
-                b64ShCmd=base64.standard_b64encode(sRunCmd)
-                sShCommand="nohup echo '" + b64ShCmd + "' | base64 -d | sh && exit\n"
-            if USERTYPES[usertype]==ROOT:
-                print "!!!!Executing as ROOT!!!!"
-                sShCommand="set +o history && echo '" + password + "' | sudo --stdin " + sShCommand
-            print "\n\n Running Command:" + sShCommand +"\n\n"
-            remote_conn.send(sShCommand)
-            #remote_conn.send("sh " + os.path.join(TEMP_DIR,sTmpFileName) + "\n")
-            # Wait for the command to complete
-            time.sleep(1)
-            #remote_conn.send(password + "\n")
-            # Wait for the command to complete
-            #time.sleep(3)
-            output = remote_conn.recv(5000)
-            print "Client Returned:", output
-        except Exception as e: 
-            print "EXCEPTION for IP: ",ip
-            print(e)
-            traceback.print_exc()
-            #print "!!!! Error connecting to " + ip
-            continue
+def do_ssh_runcmd(ip,sScript,sTmpFileName,retries=5):
+
+    runlog=[]
+    runlog.append("########### IP:" + ip)
+    username,password,usertype = get_creds(USER_CONFIG)
+
+    # Create instance of SSHClient object
+    remote_conn_pre = paramiko.SSHClient() #patched_SSHClient() #paramiko.
+
+    # Automatically add untrusted hosts (make sure okay for security policy in your environment)
+    remote_conn_pre.set_missing_host_key_policy(
+         paramiko.AutoAddPolicy())
+
+    try:
+
+        # initiate SSH connection
+        runlog.append("Starting SSH connection with " + ip)
+        retry_count = 0
+        while retry_count < retries:
+            try:
+                isSuccessful=remote_conn_pre.connect(ip, username=username, look_for_keys=True, allow_agent=False, timeout=5)
+                #remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=True, allow_agent=False, timeout=1)
+                #remote_conn_pre.connect(ip, username=username, )#key_filename="/Users/vp/.ssh/id_rsa.pub"
+                #self._chan = self.ssh_connection.invoke_shell(width=9999999,
+                #                                              height=9999999)
+                
+                if isSuccessful is not None:
+                    break
+            except (paramiko.SSHException,socket.error), msg:
+                retry_count = retry_count + 1
+                runlog.append("Connect attempt {0} failed: {1}".format(
+                    retry_count,
+                    msg))
+            except paramiko.BadHostKeyException, e:
+                raise BadHostKeyError(e.hostname, e.key, e.expected_key)
+            except paramiko.AuthenticationException, e:
+                raise AuthenticationError()
+
+        if retry_count >= retries:
+            runlog.append("Too many connection attempts for " + ip)
+            return
+            
+        runlog.append("SSH connection established to " + ip)
+
+        # Use invoke_shell to establish an 'interactive session'
+        remote_conn = remote_conn_pre.invoke_shell()
+        runlog.append("Interactive SSH session established")
+
+        # Strip the initial router prompt
+        output = remote_conn.recv(1000)
+
+        # See what we have
+        runlog.append(output)
+
+        # Turn off paging
+        #disable_paging(remote_conn)
+
+        remote_conn.send("\n")
+        if USE_TEMP_FILE:
+            bInMultiLineComment=False
+            for sLine in sScript:
+                if sLine.startswith("'''"): 
+                    bInMultiLineComment= not bInMultiLineComment
+                if not sLine.startswith("#") and not sLine.startswith("'''") and not len(sLine.strip())<=1 and bInMultiLineComment==False:
+                    # Now let's try to send a command
+                    runlog.append("Sending: " + "echo '" + sLine.strip() + "' >> " + os.path.join(TEMP_DIR,sTmpFileName))
+                    remote_conn.send("echo '" + sLine.strip() + "' >> " + os.path.join(TEMP_DIR,sTmpFileName) + "\n")
+
+                    # Wait for the command to complete
+                    time.sleep(.3)
+                
+                    output = remote_conn.recv(5000)
+                    runlog.append(output)
+        
+            sShCommand="nohup sh " + os.path.join(TEMP_DIR,sTmpFileName) + " && rm " + os.path.join(TEMP_DIR,sTmpFileName) + " && exit\n"
+        else:
+            #send the date command as the first command so that you know when it was started.
+            sRunCmd="date"
+            for sLine in sScript:
+                if not sLine.startswith("#") and not len(sLine.strip())<=1:
+                    sRunCmd=sRunCmd + " && " + sLine.strip()
+            #Due the variety of escapes and things in scripts you need to base64 it before sending it over
+            b64ShCmd=base64.standard_b64encode(sRunCmd)
+            sShCommand="nohup echo '" + b64ShCmd + "' | base64 -d | sh && exit\n"
+        if USERTYPES[usertype]==ROOT:
+            runlog.append("!!!!Executing as ROOT!!!!")
+            sShCommand="set +o history && echo '" + password + "' | sudo --stdin " + sShCommand
+        runlog.append( "\n\n Running Command:" + sShCommand +"\n\n")
+        remote_conn.send(sShCommand)
+        #remote_conn.send("sh " + os.path.join(TEMP_DIR,sTmpFileName) + "\n")
+        #stdin, stdout, stderr = ssh.exec_command(cmd)
+        #stdin.write('xy\n')
+        #stdin.flush()
+        # Wait for the command to complete
+        time.sleep(1)
+        #remote_conn.send(password + "\n")
+        # Wait for the command to complete
+        #time.sleep(3)
+        output = remote_conn.recv(5000)
+        runlog.append("Client Returned:"+ output)
+    except Exception as e: 
+        runlog.append( "EXCEPTION for IP: "+ ip)
+        runlog.append(e)
+        #runlog.append(traceback.print_exc())
+        runlog.append(traceback.format_exc())
+        #print "!!!! Error connecting to " + ip
+        #continue
+    finally:
+        with outlock:
+            print "#" * 75
+            for line in runlog:
+                print line
 
 if __name__ == '__main__':
-    do_ssh_runcmd()
-    
-'''
-	
-    You'll need to put the calls into separate threads (or processes, but that would be overkill) which in turn requires the code to be in a function (which is a good idea anyway: don't have substantial code at a module's top level).
-
-    For example:
-
-    import sys, os, string, threading
-    import paramiko
-
-    cmd = "grep -h 'king' /opt/data/horror_20100810*"
-
-    outlock = threading.Lock()
-
-    def workon(host):
-
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, username='xy', password='xy')
-        stdin, stdout, stderr = ssh.exec_command(cmd)
-        stdin.write('xy\n')
-        stdin.flush()
-
-        with outlock:
-            print stdout.readlines()
-
-    def main():
-        hosts = ['10.10.3.10', '10.10.4.12', '10.10.2.15', ] # etc
-        threads = []
-        for h in hosts:
-            t = threading.Thread(target=workon, args=(h,))
-            t.start()
-            threads.append(t)
-        for t in threads:
-            t.join()
-
-    main()
-    '''
+    Exec_per_ip()
